f142639891bc4d2e1e5f6dbff3a8fa8c347f6090
Simon Clucas <snclucas@gmail.com>
2018-02-27 14:20:29 -0500

    Added neo4j

--- src/main/java/com/hashtagplus/App.java
+++ src/main/java/com/hashtagplus/App.java
@@ -13,12 +13,14 @@ import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.Map;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
 public class App {
 
     private static Log logger = LogFactory.getLog(App.class);
--- src/main/java/com/hashtagplus/model/HtplUser.java
+++ src/main/java/com/hashtagplus/model/HtplUser.java
@@ -1,24 +1,43 @@
 package com.hashtagplus.model;
 
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
+@NodeEntity
 public class HtplUser extends User {
 
-    private String id;
+  @GraphId
+  private String id;
 
-    public HtplUser(String username, String password, String id, Collection<? extends GrantedAuthority> authorities) {
-        super(username, password, authorities);
-        this.id = id;
-    }
+  @Relationship(type = "TEAMMATE", direction = Relationship.UNDIRECTED)
+  public Set<HtplUser> friends;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
+
+  public HtplUser(String username, String password, String id, Collection<? extends GrantedAuthority> authorities) {
+    super(username, password, authorities);
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void addFriend(HtplUser person) {
+    if (friends == null) {
+      friends = new HashSet<>();
     }
+    friends.add(person);
+  }
 }
